1876b93d735597374ab18c587b8120fcbea034f4
mapldx <git@fwd.zacharymail.com>
2024-04-04 00:36:38 -0400

    feat: show interactive spinners with info to log progress

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import path from 'path';
 import { md_handler, html_handler } from './utils/transmogrify.js';
 import { talk_to_ai } from './utils/ai.js';
 import inquirer from 'inquirer';
+import ora, { spinners } from 'ora';
 
 const program = new Command();
 process.removeAllListeners('warning');
@@ -24,6 +25,8 @@ program.parse(process.argv);
 const options = program.opts();
 
 async function parse_path(directory, target = 'package.json') {
+  // let spinner = ora('Looking for a valid package.json').start();
+  // spinner.color = 'blue';
   if (directory === undefined) {
     console.log('Please provide a path to your project directory');
     process.exit(1);
@@ -38,44 +41,72 @@ async function parse_path(directory, target = 'package.json') {
       }
     }
     if (target === file.name) {
+      // spinner.succeed('Found a valid package.json');
       return [path.join(directory, file.name)];
     }
   }
+  // spinner.fail('No package.json found');
   return undefined;
 }
 
 async function parse_entrypoint(package_path, base_url) {
+  let spinner = ora('Reading package.json for a project entrypoint').start();
+  spinner.color = 'blue';
   try {
     const file = await fs.readFile(package_path, 'utf8');
     const entrypoint = JSON.parse(file).main;
     if (!entrypoint) {
-      console.log('No entrypoint found');
+      // console.log('No entrypoint found');
+      spinner.fail('No entrypoint found in package.json (main field)');
       process.exit(1);
     }
     // console.log('Entrypoint:', entrypoint);
     const entrypoint_path = path.join(path.dirname(package_path), entrypoint);
+    spinner.succeed('Found a valid entrypoint at ' + entrypoint_path);
+    spinner = ora('Reading entrypoint for API endpoints').start();
     const data = await fs.readFile(entrypoint_path, 'utf8');
     // console.log('Data:', data);
     let fn = false;
     let content = '';
     let responses = [];
+    let endpoint = '';
+    let has_endpoint = false;
     for (let line of data.split('\n')) {
       if (fn) {
         content += line + '\n';
         if (line === '});' || line === '})') {
-          responses.push(await talk_to_ai(base_url, content + '});'));
+          if (!has_endpoint) {
+            spinner.succeed('Found at least one API endpoint');
+            has_endpoint = true;
+          }
+          // handle multiple return values from talk_to_ai namely message and spinner
+          spinner = ora('Talking to AI for documentation on ' + endpoint).start();
+          let message = await talk_to_ai(base_url, content);
+          if (message == null) {
+            spinner.info('Received an invalid response from the AI, automatically retrying...');
+            spinner = ora('Talking to AI').start();
+            message = await talk_to_ai(base_url, content);
+          } else {
+            spinner.succeed('AI has responded for ' + endpoint);
+            responses.push(message);
+          }
           fn = false;
           content = '';
         }
       } else if (line.match(/app\.(get|post|put|delete)\(/)) {
         fn = true;
+        endpoint = line;
         content = line + '\n';
       }
     }
     // console.log('Responses:', responses);
+    if (spinner.isSpinning) {
+      spinner.succeed('AI has responded');
+    }
     return responses;
   } catch (error) {
-    console.error("Error:", error);
+    spinner.fail('An unknown error occurred while reading the entrypoint or talking to AI');
+    // console.error("Error:", error);
     process.exit(1);
   }
 }
@@ -104,21 +135,36 @@ async function parse_entrypoint(package_path, base_url) {
         choices: ['JSON (.json)', 'Markdown (.md)', 'Simple HTML (.html)']
       }
     ]).then(async answers => {
+      console.log("\n");
+      let spinner = ora('Looking for a valid package.json').start();
+      spinner.color = 'blue';
       const [package_path] = await parse_path(answers.path);
+      if (package_path === undefined) {
+        spinner.fail('No package.json found');
+        process.exit(1);
+      }
       if (package_path) {
+        spinner.succeed('Found a valid package.json');
         let responses = await parse_entrypoint(package_path, answers.baseUrl);
+        spinner = ora('Writing API documentation').start();
+        spinner.color = 'blue';
         const output = path.join(answers.path, 'api-documentation.json');
         await fs.writeFile(output, JSON.stringify(responses, null, 2));
-        console.log('API documentation generated successfully');
+        // spinner.succeed('Successfully written default JSON output to + ' + output);
+        // console.log('API documentation generated successfully');
         if (answers.target) {
-          console.log('Target format:', answers.target);
+          // console.log('Target format:', answers.target);
+          // if (answers.target !== "JSON (.json)") {
+          //   spinner = ora('Transmogrifying API documentation to ' + answers.target).start();
+          // }
           if (answers.target === "Markdown (.md)") {
             await md_handler(output, answers.path);
           } else if (answers.target === "Simple HTML (.html)") {
             await html_handler(output, answers.path);
           } else if (answers.target === "JSON (.json)") {
-            console.log('JSON output saved to:', output);
+            // console.log('JSON output saved to:', output);
           }
+          spinner.succeed('Successfully transmogrified API documentation to ' + answers.target);
         } else {
           console.log('No target format specified');
           process.exit(1);
