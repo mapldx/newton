d5bbcf6cb422bbbc18587e821892b9bb4c3be8e9
mapldx <git@fwd.zacharymail.com>
2024-04-04 21:00:00 -0400

    feat: mockup layout for generated documentation in Next

---
+++ src/lib/src/app/components/code.js
@@ -0,0 +1,28 @@
+'use client';
+
+import { CopyBlock, dracula } from 'react-code-blocks';
+
+export default function Code({ header, code, language }) {
+    return (
+        <div>
+            <div class="font-mono bg-gray-400 px-4 py-1 rounded-t-lg text-slate-200 font-semibold text-sm">
+                {header}
+            </div>
+            <div class="bg-gray-500 rounded-b-lg">
+                <CopyBlock
+                    text={code}
+                    language={language}
+                    showLineNumbers={true}
+                    theme={dracula}
+                    customStyle={{
+                        paddingLeft: '1rem',
+                        paddingRight: '1rem',
+                        boxShadow: '1px 2px 3px rgba(0,0,0,0.35)',
+                        fontSize: '0.8rem',
+                    }}
+                    wrapLongLines={true}
+                />
+            </div>
+        </div>
+    );
+}
